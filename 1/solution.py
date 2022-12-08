755e36e39fdb238366272340ae05e59d09027c21
Logan Davidson <logan.davidson@autorek.com>
2022-12-08 14:06:17 +0000

    Add solution to day 1 puzzle

---
+++ 1/solution.py
@@ -0,0 +1,24 @@
+with open('inputData.txt') as f:
+    lines = f.readlines()
+
+totals = []
+
+for line in lines:
+    if totals == []:
+        totals.append(int(line))
+    elif line.isspace():
+        totals.append(0)
+    else:
+        totals[-1] += int(line)
+
+topTotals = []
+topFrequency = 3
+
+for i in range(topFrequency):
+    maxValue = max(totals)
+    print("Top elf #{n} has {c} calories".format(n=i+1, c=maxValue))
+    topTotals.append(maxValue)
+    totals.remove(maxValue)
+
+sumTopTotals = sum(topTotals)
+print("Top {f} elves have a total of {c} calories".format(f=topFrequency, c=sumTopTotals))
