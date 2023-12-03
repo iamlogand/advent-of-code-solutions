ec168b185709a07a3c2950efbe9f51413b10c961
Logan Davidson <iamlogandavidson@gmail.com>
2023-12-03 09:20:51 +0000

    Rework my solution to day 3 part 1
    Rework my solution to 2023 day 3 part 1 so that it could be adapted to also solve part 2 more easily.

--- 2023/03/solution.py
+++ 2023/03/solution.py
@@ -2,75 +2,84 @@ from typing import List
 
 
 class Point:
-    def __init__(self, x: int, y: int):
-        self.x = x
+    def __init__(self, y: int, x: int):
         self.y = y
+        self.x = x
+
+
+class Symbol:
+    def __init__(self, character: str, position: Point):
+        self.character: str = character
+        self.position: Point = position
 
 
 class PartNumber:
-    def __init__(self, number: int, zone_start: Point, zone_end: Point):
+    def __init__(self, number: int, start: Point, end: Point):
         self.number: int = number
-
-        # The "zone" is the area around the part number where a symbol would be considered adjacent.
-        # These are represented by start (top left) and end (bottom right) coordinates.
-        self.zone_start: Point = zone_start
-        self.zone_end: Point = zone_end
+        self.start: Point = start
+        self.end: Point = end
 
 
-def check_for_symbol(schematic, zone_start: (int, int), zone_end: (int, int)):
-    max_y = len(schematic) - 1
-    max_x = len(schematic[0]) - 1
+def get_adjacent_part_numbers(
+    symbol: Symbol, part_nums: List[PartNumber]
+) -> (List[PartNumber], List[PartNumber]):
+    matches = []
+    rejects = []
 
-    for y in range(zone_start.y, zone_end.y + 1):
-        for x in range(zone_start.x, zone_end.x + 1):
-            if y < 0 or x < 0 or y >= max_y or x >= max_x:
-                continue
-            char = schematic[y][x]
-            if not char.isnumeric() and char != ".":
-                return True
-    return False
+    for part in part_nums:
+        # check adjacency
+        is_y_adjacent = part.start.y - 1 <= symbol.position.y <= part.end.y + 1
+        is_x_adjacent = part.start.x - 1 <= symbol.position.x <= part.end.x + 1
+        if is_y_adjacent and is_x_adjacent:
+            matches.append(part)
+        else:
+            rejects.append(part)
+
+    return (matches, rejects)
 
 
-def get_part_numbers(line: str, y: int) -> List[PartNumber]:
+def parse_schematic(line: str, y: int) -> (List[PartNumber], List[Symbol]):
     part_nums = []
+    symbols = []
     current_num = ""
     width = len(line)
     for i in range(width + 1):
-        if i != width and line[i] in "1234567890":
+        if i != width and line[i].isnumeric():
             current_num += line[i]
         elif current_num != "":
-            zone_start = Point(i - len(current_num) - 1, y - 1)
-            zone_end = Point(i, y + 1)
-            part_nums.append(PartNumber(int(current_num), zone_start, zone_end))
+            start = Point(y, i - len(current_num))
+            end = Point(y, i - 1)
+            part_nums.append(PartNumber(int(current_num), start, end))
             current_num = ""
-    return part_nums
+
+        if i != width and not line[i].isnumeric() and line[i] != ".":
+            position = Point(y, i)
+            symbols.append(Symbol(line[i], position))
+    return part_nums, symbols
 
 
-def sum_all_part_numbers(schematic: str) -> int:
+def solve_problem(schematic: str) -> int:
     part_nums = []
+    symbols = []
     y = 0
     for line in schematic:
-        part_nums += get_part_numbers(line.strip(), y)
+        new_part_nums, new_symbols = parse_schematic(line.strip(), y)
+        part_nums = part_nums + new_part_nums
+        symbols = symbols + new_symbols
         y += 1
 
     sum = 0
-    for part_num in part_nums:
-        is_near_symbol = check_for_symbol(
-            schematic, part_num.zone_start, part_num.zone_end
+    for symbol in symbols:
+        matching_part_nums, remaining_part_nums = get_adjacent_part_numbers(
+            symbol, part_nums
         )
-        if is_near_symbol:
+        for part_num in matching_part_nums:
             sum += part_num.number
-
-        # print(
-        #     part_num.number,
-        #     (part_num.zone_start.x, part_num.zone_start.y),
-        #     (part_num.zone_end.x, part_num.zone_end.y),
-        #     is_near_symbol,
-        # )
+        part_nums = remaining_part_nums
     return sum
 
 
 with open("2023/03/input.txt") as input:
     schematic = input.readlines()
 
-    print("Solution for part 1: {}".format(sum_all_part_numbers(schematic)))
+    print("Solution for part 1: {}".format(solve_problem(schematic)))
