09fdf2ca5465336d6ed9b059933e191766fc9a7b
Logan Davidson <logan.davidson@autorek.com>
2023-12-14 15:21:47 +0000

    Better solution to 2023 day 12
    This solution is much faster than my last solution, but it's still pretty slow. It can solve the problem with the test case input in a reasonable time frame, but it's too slow to solve the problem with the real input data.

--- 2023/12/solution.py
+++ 2023/12/solution.py
@@ -1,86 +1,87 @@
 from typing import List
-import itertools
 
 
-def generate_combinations(target, input_count, max_num=None, prefix=None):
-    combinations = []
-    for combination in itertools.product(range(2), repeat=input_count):
-        if sum(combination) == target:
-            combinations.append(combination)
-    return combinations
-
-
-def generate_arrangement(damaged_counts: List[int], padding: List[int] = []) -> str:
-    possible_arrangement = ""
-    first = True
-    for i in range(len(damaged_counts)):
-        count = damaged_counts[i]
-        if not first:
-            possible_arrangement += "."
-        if i < len(padding):
-            possible_arrangement += "." * padding[i]
-        first = False
-        possible_arrangement += "#" * count
-        if i + 1 == len(damaged_counts) and i + 2 == len(padding):
-            possible_arrangement += "." * padding[i + 1]
-    return possible_arrangement
-
-
-def sum_variants(input_arrangement: str, damaged_counts: List[int]):
-    unknowns = list(input_arrangement).count("?")
-    total_damaged = sum(damaged_counts)
-    known_damaged = list(input_arrangement).count("#")
-    possible_unknowns = generate_combinations(
-        total_damaged - known_damaged, unknowns)
-    arrangement_count = 0
-    for possible_unknown in possible_unknowns:
-        candidate_arrangement = ""
-        sub_index = 0
-        for char in input_arrangement:
-            if char == "?":
-                candidate_arrangement += "#" if possible_unknown[sub_index] == 1 else "."
-                sub_index += 1
+def validate_candidate(candidate: str, expected_damaged: List[int], input_arrangement: str) -> bool:
+    damage_count = 0
+    for char in candidate:
+        if char == "#":
+            damage_count += 1
+    expected_damage_count = sum(expected_damaged)
+    remaining = len(input_arrangement) - len(candidate)
+    if damage_count + remaining < expected_damage_count or damage_count > expected_damage_count:
+        return False
+    candidate_damaged_counts = []
+    parsing_group = False
+    for i in range(len(candidate)):
+        expected_char = input_arrangement[i]
+        if candidate[i] != expected_char and expected_char != "?":
+            return False
+        if candidate[i] == "#":
+            if parsing_group:
+                candidate_damaged_counts[-1] += 1
             else:
-                candidate_arrangement += char
-        arrangement_is_valid = True
-        candidate_damaged_counts = []
-        parsing_group = False
-        for i in range(len(candidate_arrangement)):
-            expected_char = input_arrangement[i]
-            if candidate_arrangement[i] != expected_char and expected_char != "?":
-                arrangement_is_valid = False
-                break
-            if candidate_arrangement[i] == "#":
-                if parsing_group:
-                    candidate_damaged_counts[-1] += 1
-                else:
-                    parsing_group = True
-                    candidate_damaged_counts.append(1)
-            else:
-                if parsing_group:
-                    parsing_group = False
-            if not parsing_group and candidate_damaged_counts != damaged_counts[:len(candidate_damaged_counts)]:
-                arrangement_is_valid = False
-                break
-        if candidate_damaged_counts == damaged_counts and arrangement_is_valid:
-            arrangement_count += 1
-            print(".", end="")
-    print()
-    return arrangement_count
+                parsing_group = True
+                candidate_damaged_counts.append(1)
+        else:
+            if parsing_group:
+                parsing_group = False
+        if not parsing_group and candidate_damaged_counts != expected_damaged[:len(candidate_damaged_counts)]:
+            return False
+    if len(candidate) == len(input_arrangement) and candidate_damaged_counts != expected_damaged:
+        return False
+    return True
+
+
+def guess_answer(input_arrangement: str, candidates: List[str], expected_damaged: List[int], this_candidate: str = "", char_index: int = 0) -> List[str]:
+    candidate_is_valid = validate_candidate(
+        this_candidate, expected_damaged, input_arrangement)
+    if not candidate_is_valid:
+        return
+    if char_index >= len(input_arrangement):
+        candidates.append(this_candidate)
+        return
+    this_char = input_arrangement[char_index]
+    if this_char == ".":
+        guess_answer(
+            input_arrangement, candidates, expected_damaged,
+            this_candidate + ".", char_index + 1)
+        return
+    elif this_char == "#":
+        guess_answer(
+            input_arrangement, candidates, expected_damaged,
+            this_candidate + "#", char_index + 1)
+        return
+    else:
+        guess_answer(
+            input_arrangement, candidates, expected_damaged,
+            this_candidate + ".", char_index + 1)
+        guess_answer(
+            input_arrangement, candidates, expected_damaged,
+            this_candidate + "#", char_index + 1)
+        return
+
+
+def sum_variants(input_arrangement: str, expected_damaged: List[int]) -> int:
+    candidates = []
+    guess_answer(input_arrangement, candidates, expected_damaged)
+    return len(candidates)
 
 
 with open("2023/12/input.txt") as input:
     lines = [line.strip() for line in input.readlines()]
 
+print("SOLVING PART 1\n")
 variant_sum = 0
 for line in lines:
     input_arrangement = line.split(" ")[0]
-    damaged_counts = [int(n) for n in line.split(" ")[1].split(",")]
-    variant_sum += sum_variants(input_arrangement, damaged_counts)
+    expected_damaged = [int(n) for n in line.split(" ")[1].split(",")]
+    variant_sum += sum_variants(input_arrangement, expected_damaged)
 print("Solution for part 1: {}".format(variant_sum))
 
+print("\nSOLVING PART 2\n")
 variant_sum = 0
 for line_index in range(len(lines)):
+    print(f"Solving line {line_index + 1} of {len(lines)}")
     line = lines[line_index]
     split_input_arrangement = [line.split(" ")[0]] * 5
     input_arrangement = ""
@@ -90,5 +91,4 @@ for line_index in range(len(lines)):
         input_arrangement += split_input_arrangement[i]
     damaged_counts = [int(n) for n in line.split(" ")[1].split(",")] * 5
     variant_sum += sum_variants(input_arrangement, damaged_counts)
-    print(f"Solved line {line_index + 1} of {len(lines)}")
-print("Solution for part 2: {}".format(variant_sum))
+print("\nSolution for part 2: {}".format(variant_sum))
