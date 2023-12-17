cb12fb684320c5332a5f3f2e876fc1de47005bbb
Logan Davidson <iamlogandavidson@gmail.com>
2023-12-17 16:30:56 +0000

    Add a good solution to day 12

--- 2023/12/solution.py
+++ 2023/12/solution.py
@@ -1,140 +1,81 @@
-from typing import List
+from functools import cache
 import time
-from collections import deque
+from typing import List
 
 
-def find_combinations(
-    remaining_input: str,
-    remaining_damaged: List[int],
-    this_candidate: deque,
-    candidates: int = 0,
-    current_group_index: int = 0,
-) -> List[str]:
-    if remaining_input == "":
-        return candidates + 1
-    this_char = remaining_input[0]
-    remaining_input = remaining_input[1:]
-    if this_char == "?":
-        allow_path_1 = allow_path_2 = True
+"""
+I couldn't solve the problem myself - my solution is based on another solution I found online,
+and this is my first exposure to dynamic programming.
 
-        # If there are no remaining damaged
-        # then force write of .s
-        if allow_path_2 and not remaining_damaged:
-            allow_path_2 = False
+This solution uses a dynamic programming. The problem is broken down into sub-problems,
+where each sub-problem is solved by the `sum_variants` function once.
+The function has been setup to take repeatable arguments
+and it uses memoization to cache results for a specific combination of arguments.
+This approach prevents the same sub-problem from being solved more than once,
+which saves loads of time.
+"""
 
-        # If the current group is greater than or equal to the first remaining damaged
-        # then force write of .s
-        if allow_path_2 and current_group_index >= remaining_damaged[0]:
-            allow_path_2 = False
 
-        # If remaining space is less than the minimum space required for damaged groups
-        # then force write of #s
-        if (
-            allow_path_1
-            and current_group_index < 1
-            and len(remaining_input)
-            < sum(remaining_damaged) + max(len(remaining_damaged) - 1, 0)
-        ):
-            allow_path_1 = False
+def sum_variants_for_line(input_arrangement: str, expected_damaged: List[int]) -> int:
+    @cache
+    def sum_variants(index: int = 0, damaged_index: int = 0, variant_count: int = 0):
+        """
+        Counts the number of valid combinations that can be formed based on the input arrangement,
+        expected damaged, current indices and the variant count.
 
-        # If part way through writing a damaged group and the end has not been reached
-        # then force write of #s
-        if (
-            allow_path_1
-            and current_group_index > 0
-            and current_group_index < remaining_damaged[0]
-        ):
-            allow_path_1 = False
+        Arguments:
+            index (int): the current index in input_arrangement.
+            damaged_index (int): the current index in expected_damaged.
+            variant_count (int): counter for valid combinations that have been found.
 
-        # Path 1 - write a . (non-damaged spring)
-        if allow_path_1:
-            if current_group_index > 0:
-                path_1_remaining = remaining_damaged[1:]
-            else:
-                path_1_remaining = remaining_damaged
-            this_candidate.append(".")
-            candidates = find_combinations(
-                remaining_input,
-                path_1_remaining,
-                this_candidate,
-                candidates,
-                -1 if current_group_index > 0 else current_group_index - 1,
-            )
-            this_candidate.pop()
-        # Path 2 - write a # (damaged spring)
-        if allow_path_2:
-            this_candidate.append("#")
-            candidates = find_combinations(
-                remaining_input,
-                remaining_damaged,
-                this_candidate,
-                candidates,
-                1 if current_group_index < 0 else current_group_index + 1,
-            )
-            this_candidate.pop()
-        return candidates
-    else:
-        if not remaining_damaged and "#" in remaining_input:
-            return candidates
-        if this_char == ".":
-            if current_group_index > 0 and current_group_index < remaining_damaged[0]:
-                return candidates
-            this_candidate.append(".")
-            candidates = find_combinations(
-                remaining_input,
-                remaining_damaged[1:] if current_group_index > 0 else remaining_damaged,
-                this_candidate,
-                candidates,
-                -1 if current_group_index > 0 else current_group_index - 1,
-            )
-            return candidates
-        else:
+        Returns:
+            int: the passed variant count plus the number of new valid combinations found.
+        """
+
+        if index == len(input_arrangement):
+            # End has been reached (returns True if valid)
+            return damaged_index == len(expected_damaged)
+
+        if input_arrangement[index] in [".", "?"]:
+            # Path for the next character being "."
+            variant_count += sum_variants(index + 1, damaged_index)
+
+        if damaged_index < len(expected_damaged):
+            damaged_end_index = index + expected_damaged[damaged_index]
+
+            # Check if it's possible to place a "#" character
+            # at the current index considering the current damaged index
             if (
-                len(remaining_damaged) == 0
-                or current_group_index >= remaining_damaged[0]
+                "." not in input_arrangement[index:damaged_end_index]
+                and damaged_end_index < len(input_arrangement)
+                and "#" not in input_arrangement[damaged_end_index]
             ):
-                return candidates
-            this_candidate.append("#")
-            candidates = find_combinations(
-                remaining_input,
-                remaining_damaged,
-                this_candidate,
-                candidates,
-                1 if current_group_index < 0 else current_group_index + 1,
-            )
-            return candidates
+                # Path for the next character being "#"
+                variant_count += sum_variants(damaged_end_index + 1, damaged_index + 1)
 
+        return variant_count
 
-def sum_variants(input_arrangement: str, expected_damaged: List[int]) -> int:
-    candidates = find_combinations(input_arrangement, expected_damaged, deque())
-    return candidates
+    return sum_variants()
 
 
 start_time = time.time()
+
 with open("2023/12/input.txt") as input:
     lines = [line.strip() for line in input.readlines()]
 
-variant_sum = 0
+sum = 0
 for line in lines:
-    input_arrangement = line.split(" ")[0]
-    expected_damaged = [int(n) for n in line.split(" ")[1].split(",")]
-    variant_sum += sum_variants(input_arrangement, expected_damaged)
-print("Answer for part 1: {}".format(variant_sum))
+    input_arrangement = line.split(" ")[0] + "."
+    expected_damaged = [int(i) for i in line.split(" ")[1].split(",")]
+    sum += sum_variants_for_line(input_arrangement, expected_damaged)
+print("Answer for part 1: {}".format(sum))
 
-variant_sum = 0
-for line_index in range(len(lines)):
-    line = lines[line_index]
-    question_count = line.count("?") * 5
-    group_count = len(line.split(" ")[1].split(",")) * 5
-    split_input_arrangement = [line.split(" ")[0]] * 5
-    input_arrangement = ""
-    for i in range(len(split_input_arrangement)):
-        if i != 0:
-            input_arrangement += "?"
-        input_arrangement += split_input_arrangement[i]
-    damaged_counts = [int(n) for n in line.split(" ")[1].split(",")] * 5
-    variant_sum += sum_variants(input_arrangement, damaged_counts)
-print("Answer for part 2: {}".format(variant_sum))
+sum = 0
+for line in lines:
+    input_arrangement = (line.split(" ")[0] + "?") * 5
+    expected_damaged = [int(i) for i in line.split(" ")[1].split(",")] * 5
+    sum += sum_variants_for_line(input_arrangement, expected_damaged)
+print("Answer for part 2: {}".format(sum))
 
 end_time = time.time()
 execution_time = int((end_time - start_time) * 1000)
